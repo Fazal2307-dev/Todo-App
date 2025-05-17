030946d74d60cdba2c2eb25dfc9f69cb43568068
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-17 22:16:02 +0530

    update and delete some file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 
 import './App.css';
-import Button from './components/Button';
-import Header from './components/Header';
-import TodoItem from './components/TodoItem';
-import "./style.css"
+
 
 function App() {
   return (
-    <div className="todo-contaainer">
-        <Header />
-        <TodoItem />
-        <Button />
-    </div>
+    <></>
   );
 }
 
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = ()=>{
-  return (
-      <h1 className='todo-header'>Todo App</h1>
-    
-  )
-}
-export default Header;

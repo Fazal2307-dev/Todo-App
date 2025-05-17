c4b5b839dccf3fb9fe94e2ed07b6e731ad147d5c
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-17 21:24:13 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from './components/Button';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import "./style.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-contaainer">
+        <Header />
+        <TodoItem />
+        <Button />
     </div>
   );
 }

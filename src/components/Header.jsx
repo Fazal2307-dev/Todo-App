2e572eedee93ead118cf9a6caa76186a5191d186
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-18 09:40:55 +0530

    veryfing

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ()=>{
+  return (
+    <>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, ullam.</p>
+    
+    </>
+  )
+}
+export default Header;
